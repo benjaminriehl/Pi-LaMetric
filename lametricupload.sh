1fdb102fa3f34f1732ed47ace023927174b78a39
JamjamRie <benjaminriehl76@gmail.com>
2017-12-04 17:18:13 -0600

    Added a static string of text before the number
    and added "Unique users"

--- lametricupload.sh
+++ lametricupload.sh
@@ -1,19 +1,26 @@
 bads=$1
+user=$2
 
 echo $bads
-QBADS=bads
+echo $user
 
 curl -X POST \
 -H "Accept: application/json" \
--H "X-Access-Token: YOUR_KEY_HERE" \
+-H "X-Access-Token: YOUR_ACCESS_TOKEN_HERE" \
 -H "Cache-Control: no-cache" \
 -d '{
     "frames": [
         {
-            "text": '$bads',
+            "text": "# of ads blocked: '$bads'",
             "icon": "i14870",
             "index": 0
+        },
+        {
+            "text": "# of unique clients: '$user'",
+            "icon": "i1776",
+            "index": 1
         }
     ]
 }' \
-https://developer.lametric.com/api/v1/dev/widget/update/com.lametric.YOUR_APP_ID_HERE/1
+
+#Add your push url on top of this line
